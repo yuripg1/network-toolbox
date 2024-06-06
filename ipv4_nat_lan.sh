39f27393c4c6c36a1bf6f71aa3098178589d6f7e
Yuri Gossmann <yuripg1@gmail.com>
2024-06-06 20:16:06 -0300

    fix: improved mangle rules

---
+++ ipv4_nat_lan.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+LAN_INTERFACE="eth1"
+DNS_SERVER_IP_ADDRESS="10.182.186.1"
+logger "Checking IPv4 NAT rules on LAN interface $LAN_INTERFACE"
+NAT_LAN_CUSTOM_001_COUNT=$(sudo iptables -t nat -L -v -n | grep -c "NAT_LAN_CUSTOM_001")
+NAT_LAN_CUSTOM_002_COUNT=$(sudo iptables -t nat -L -v -n | grep -c "NAT_LAN_CUSTOM_002")
+if [ "$NAT_LAN_CUSTOM_001_COUNT" -eq "0" ]; then
+  sudo iptables -t nat -A PREROUTING -i "$LAN_INTERFACE" -p udp --dport 53 ! -d "$DNS_SERVER_IP_ADDRESS" -j REDIRECT --to-ports 53 -m comment --comment "NAT_LAN_CUSTOM_001"
+  logger "NAT_LAN_CUSTOM_001 created"
+fi
+if [ "$NAT_LAN_CUSTOM_002_COUNT" -eq "0" ]; then
+  sudo iptables -t nat -A PREROUTING -i "$LAN_INTERFACE" -p tcp --dport 53 ! -d "$DNS_SERVER_IP_ADDRESS" -j REDIRECT --to-ports 53 -m comment --comment "NAT_LAN_CUSTOM_002"
+  logger "NAT_LAN_CUSTOM_002 created"
+fi
---
+++ ipv6_nat_lan.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+LAN_INTERFACE="eth1"
+DNS_SERVER_IP_ADDRESS="fe80::d021:f9ff:fedd:c850"
+logger "Checking IPv6 NAT rules on LAN interface $LAN_INTERFACE"
+IPV6_NAT_LAN_CUSTOM_001_COUNT=$(sudo ip6tables -t nat -L -v -n | grep -c "IPV6_NAT_LAN_CUSTOM_001")
+IPV6_NAT_LAN_CUSTOM_002_COUNT=$(sudo ip6tables -t nat -L -v -n | grep -c "IPV6_NAT_LAN_CUSTOM_002")
+if [ "$IPV6_NAT_LAN_CUSTOM_001_COUNT" -eq "0" ]; then
+  sudo ip6tables -t nat -A PREROUTING -i "$LAN_INTERFACE" -p udp --dport 53 ! -d "$DNS_SERVER_IP_ADDRESS" -j REDIRECT --to-ports 53 -m comment --comment "IPV6_NAT_LAN_CUSTOM_001"
+  logger "IPV6_NAT_LAN_CUSTOM_001 created"
+fi
+if [ "$IPV6_NAT_LAN_CUSTOM_002_COUNT" -eq "0" ]; then
+  sudo ip6tables -t nat -A PREROUTING -i "$LAN_INTERFACE" -p tcp --dport 53 ! -d "$DNS_SERVER_IP_ADDRESS" -j REDIRECT --to-ports 53 -m comment --comment "IPV6_NAT_LAN_CUSTOM_002"
+  logger "IPV6_NAT_LAN_CUSTOM_002 created"
+fi
